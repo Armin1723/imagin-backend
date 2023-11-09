4ce115100ae190dc846458f2971e78f90a967ce7
Armin1723 <110764802+Armin1723@users.noreply.github.com>
2023-11-09 19:02:49 +0530

    fixed rate-limiter

--- index.js
+++ index.js
@@ -3,20 +3,10 @@ const express = require('express')
 const dbConnection = require('./db.js')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const rateLimit = require('express-rate-limit')
 
 const app = express()
 
-const rateLimiter = rateLimit({
-  windowMs: 60 * 1000, // 24 hrs in milliseconds
-  max: 10,
-  message: 'You have exceeded the 5 requests in 1 min limit!', 
-  standardHeaders: true,
-  legacyHeaders: false,
-});
-
 app.use(express.json())
-app.use(rateLimiter)
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 
